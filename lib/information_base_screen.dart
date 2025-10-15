6ffc902ecba36b13575365e709980c21f39d4ee1
unmoral <maksn19092006@gmail.com>
2025-10-15 10:21:00 +0300

    information_base_screen_UI

--- lib/information_base_screen.dart
+++ lib/information_base_screen.dart
@@ -6,16 +6,60 @@ class KnowledgeBasePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final languages = [
+  {
+    'name': 'Dart',
+    'logo': 'https://upload.wikimedia.org/wikipedia/commons/7/7e/Dart-logo.png',
+  },
+  {
+    'name': 'Python',
+    'logo': '',
+  },
+  {
+    'name': 'JavaScript',
+    'logo': 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+  },
+  {
+    'name': 'C#',
+    'logo': 'https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png',
+  },
+  {
+    'name': 'Java',
+    'logo': 'https://img.icons8.com/color/48/000000/java-coffee-cup-logo.png',
+  },
+  {
+    'name': 'C++',
+    'logo': '',
+  },
+];
+
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('База знаний'),
         backgroundColor: AppColors.secondary,
       ),
-      body: const Center(
-        child: Text(
-          'Раздел База знаний',
-          style: TextStyle(fontSize: 24, color: Colors.white70),
-        ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: languages.length,
+        itemBuilder: (context, index) {
+          final language = languages[index];
+          return Card(
+            color: AppColors.secondary.withValues(alpha: 0.8),
+            margin: const EdgeInsets.symmetric(vertical: 8),
+            child: ListTile(
+              leading: Image.network(
+                language['logo']!,
+                width: 40,
+                height: 40,
+              ),
+              title: Text(
+                language['name']!,
+                style: const TextStyle(fontSize: 20, color: Colors.white70),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
